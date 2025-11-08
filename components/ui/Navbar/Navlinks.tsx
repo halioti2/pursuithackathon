62513ec48b6c4a8baa9b3457dc9f220e110ec292
Butter Chen <butterchen@Butters-MacBook-Pro.local>
2025-11-08 15:07:03 -0500

    Fix usePathname hook error in Navlinks component

--- components/ui/Navbar/Navlinks.tsx
+++ components/ui/Navbar/Navlinks.tsx
@@ -14,6 +14,7 @@ interface NavlinksProps {
 
 export default function Navlinks({ user }: NavlinksProps) {
   const router = getRedirectMethod() === 'client' ? useRouter() : null;
+  const pathname = usePathname();
 
   return (
     <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
@@ -36,7 +37,7 @@ export default function Navlinks({ user }: NavlinksProps) {
       <div className="flex justify-end space-x-8">
         {user ? (
           <form onSubmit={(e) => handleRequest(e, SignOut, router)}>
-            <input type="hidden" name="pathName" value={usePathname()} />
+            <input type="hidden" name="pathName" value={pathname} />
             <button type="submit" className={s.link}>
               Sign out
             </button>
