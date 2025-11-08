79d49d8e6bd16392cb3957ba001365f0f0d2c7bb
Butter Chen <butterchen@Butters-MacBook-Pro.local>
2025-11-08 15:13:53 -0500

    Change logo to mail envelope icon

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -7,13 +7,17 @@ const Logo = ({ ...props }) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <rect width="100%" height="100%" rx="16" fill="white" />
+    {/* Mail envelope icon */}
+    <rect x="4" y="8" width="24" height="16" rx="2" stroke="white" strokeWidth="2" fill="none" />
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-      fill="black"
+      d="M4 10L16 18L28 10"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
+    {/* Optional: Add a small dot indicator */}
+    <circle cx="25" cy="11" r="3" fill="#22c55e" />
   </svg>
 );
 
